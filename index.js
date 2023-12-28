9b0846b0f14e5906366f88800a895f0df3352672
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-12-28 16:28:08 -0300

    se cambio img 24hs

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const listNav = document.querySelector('#navbarSupportedContent');
 const items = document.querySelectorAll('.nav-link');
 
 
-console.log(items)
-
-
 items.forEach(item => {
     item.addEventListener('click', () => {
         listNav.classList.toggle('show')
